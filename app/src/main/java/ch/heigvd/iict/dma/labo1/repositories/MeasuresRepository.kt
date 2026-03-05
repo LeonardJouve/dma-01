6a2c2a0222737d10155b88eb7efe33adde666a1d
unknown <loris.marzullo@gmail.com>
2026-03-05 09:58:23 +0100

    Deserialization XML

--- app/src/main/java/ch/heigvd/iict/dma/labo1/repositories/MeasuresRepository.kt
+++ app/src/main/java/ch/heigvd/iict/dma/labo1/repositories/MeasuresRepository.kt
@@ -6,16 +6,16 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.map
 import ch.heigvd.iict.dma.labo1.models.*
 import ch.heigvd.iict.dma.protobuf.MeasuresOuterClass
-import com.google.gson.internal.GsonBuildConfig
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.jdom2.DocType
 import org.jdom2.Document
 import org.jdom2.Element
+import org.jdom2.input.SAXBuilder
 import org.jdom2.output.Format
 import org.jdom2.output.XMLOutputter
-import java.io.StringWriter
+import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.HttpURLConnection.HTTP_OK
 import java.net.URL
@@ -160,19 +160,40 @@ class MeasuresRepository(private val scope : CoroutineScope,
                 } else {
                     connection.getInputStream()
                 }
-/*
-                inputStream.use {
-                    val measuresAck = MeasuresOuterClass.MeasuresAck.newBuilder().mergeFrom(it).build()
-                    measuresAck.measuresList.forEach { ackMeasure ->
-                        _measures.value?.find { m -> m.id == ackMeasure.id }?.let {
-                            it.status = when (ackMeasure.status) {
-                                MeasuresOuterClass.Status.OK -> Measure.Status.OK
-                                MeasuresOuterClass.Status.NEW -> Measure.Status.NEW
-                                else -> Measure.Status.ERROR
+
+                when (serialisation) {
+                    Serialisation.PROTOBUF -> {
+
+                        inputStream.use {
+                            val measuresAck =
+                                MeasuresOuterClass.MeasuresAck.newBuilder().mergeFrom(it).build()
+                            measuresAck.measuresList.forEach { ackMeasure ->
+                                _measures.value?.find { m -> m.id == ackMeasure.id }?.let {
+                                    it.status = when (ackMeasure.status) {
+                                        MeasuresOuterClass.Status.OK -> Measure.Status.OK
+                                        MeasuresOuterClass.Status.NEW -> Measure.Status.NEW
+                                        else -> Measure.Status.ERROR
+                                    }
+                                }
                             }
                         }
                     }
-                }*/
+                    Serialisation.JSON -> {
+                        // TODO
+                    }
+                    Serialisation.XML -> {
+
+                        val saxBuilder = SAXBuilder()
+                        saxBuilder.setFeature("http://xml.org/sax/features/external-general-entities", false)
+
+                        saxBuilder.build(InputStreamReader(inputStream))
+                            .rootElement
+                            .getChildren("measure")
+                            .associate {
+                                it.getAttributeValue("id").toInt() to Measure.Status.valueOf(it.getAttributeValue("status"))
+                            }
+                    }
+                }
             }
 
             _requestDuration.postValue(elapsed)
